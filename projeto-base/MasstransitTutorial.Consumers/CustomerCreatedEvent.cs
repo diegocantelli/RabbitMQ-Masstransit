0206223dd092aa188c231b7a444152908c08a9da
Diego Cantelli <diegocantelli19@gmail.com>
2021-10-31 14:23:17 -0300

    Criando o projeto que irá consumir as mensagens da fila

---
+++ projeto-base/MasstransitTutorial.Consumers/CustomerCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MasstransitTutorial.Consumers
+{
+    public interface CustomerCreatedEvent
+    {
+        string CustomerId { get; }
+        string Name { get; }
+        DateTime Birthdate { get; }
+        string Type { get; }
+        DateTime CreatedAt { get; }
+    }
+}
---
+++ projeto-base/MasstransitTutorial.Consumers/Program.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using System;
+
+namespace MasstransitTutorial.Consumers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.ReceiveEndpoint("");
+            })
+        }
+    }
+}
